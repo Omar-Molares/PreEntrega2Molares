e3eebb053ea5d0a96c1b7e69da02a738bd36479a
Omar Molares <omarmolares1984@gmail.com>
2024-10-30 17:08:52 -0300

    arreglo de error

--- src/componentes/Checkout/Checkout.jsx
+++ src/componentes/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { useState, useEffect } from 'react'
 import { CarritoContext } from "../../context/CarritoContext"
 import { db } from '../../services/config'
